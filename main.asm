5b06dc608833a62b200e818127eeff0626143903
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2023-04-22 14:47:00 -0300

    14 bytes, not 2

--- main.asm
+++ main.asm
@@ -6,7 +6,7 @@ _start:
     mov eax, 4 ; id 4 syscall write
     mov ebx, 1 ; fd stdout
     mov ecx, dword (msg) ; msg address (buffer)
-    mov edx, 14 ; 2 bytes write
+    mov edx, 14 ; 14 bytes write
     int 0x80 ; syscall (interrupt)
     mov eax, 1 ; id 4 syscall exit
     xor ebx, ebx ; set to zero the exitcode
